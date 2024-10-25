23a578ca6f6901ce90940c7fa19920754fdf4a02
keily Lopez <klopezh16@miumg.edu.gt>
2024-10-24 19:16:25 -0600

    Publicaciones parte de su logica pero la pantalla no esta diseñada al 100

---
+++ Viajeapp/app/src/main/java/com/MarvinGudiel/viajeapp/Fragmentos/Publicacion.kt
@@ -0,0 +1,10 @@
+package com.MarvinGudiel.viajeapp.Fragmentos
+
+data class Publicacion(
+    val id: String = "",
+    val titulo: String = "",
+    val descripcion: String = "",
+    val imagenUrl: String? = null,
+    val categoria: String = ""
+)
+
