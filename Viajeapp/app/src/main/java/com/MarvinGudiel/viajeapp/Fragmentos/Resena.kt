513c58f68ba54f7421ab6e3704c7396ab4e0291c
keilylopez22 <135383970+keilylopez22@users.noreply.github.com>
2024-11-02 08:06:40 -0600

    Update Resena.kt

--- Viajeapp/app/src/main/java/com/MarvinGudiel/viajeapp/Fragmentos/Resena.kt
+++ Viajeapp/app/src/main/java/com/MarvinGudiel/viajeapp/Fragmentos/Resena.kt
@@ -1,5 +1,8 @@
 package com.MarvinGudiel.viajeapp.Fragmentos
 
 data class Resena(
-    val texto:String =""
+    val texto: String = "",
+    val rating: Int = 0,
+    val nombreUsuario: String = "",
+    val fotoPerfilUrl: String = ""
 )
